cc9a45dd847e9d033e54f459e426539b120f7a4a
chepilot <liruizenus@gmail.com>
2019-01-22 04:16:06 -0500

    POutput setup with bounding issue

--- MyProject1/Components/POutput.cs
+++ MyProject1/Components/POutput.cs
@@ -16,65 +16,83 @@ using Grasshopper.Kernel.Attributes;
 using Lab_Mouse.Properties;
 using Lab_Mouse.Components;
 
-
-
-
 namespace Lab_Mouse.Components
 {
-
-    public class PSlider : Grasshopper.Kernel.Special.GH_NumberSlider
+    public class POutput : GH_Param<GH_Integer>
     {
-        /// <summary>
-        /// Each implementation of GH_Component must provide a public 
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
-        /// new tabs/panels will automatically be created.
-        /// </summary>
-        /// 
-
+        /// Initializes a new instance of the MyComponent1 class.
         public List<double> probabilities;
-        // Rui 
-        // Temporary storage for PD
-        public string tempPD = "0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1"; // default starting string, need to be the same as the default starting probability distribution
+        // default starting string, need to be the same as the default starting probability distribution
+        public string tempPD = "0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1"; 
         public float max;
         public float min;
         public string draw_flag;
-        public PSlider()
 
-          : base()
+        public POutput()
+          : base(new GH_InstanceDescription("PDF Output", "POutput",
+              "bla bla",
+              "Lab Mouse", "Modeling"))
         {
-            base.Name = "PDF Slider";
-            base.NickName = "PSlider";
-            base.Description = "bla bla ";
-            base.Category = "Lab Mouse";
-            base.SubCategory = "Modeling";
-
-            this.probabilities = new List<double> { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }; // default starting distribution
-
-            max = (float)(this.Slider.Maximum);
-            min = (float)(this.Slider.Minimum);
-
+            // default starting distribution
+            this.probabilities = new List<double> { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 }; 
             this.draw_flag = "h";
         }
-
 
         public override GH_Exposure Exposure
         {
             get;
-            // { return GH_Exposure.primary; }
         }
 
-        public override void CreateAttributes()
+        /// Provides an Icon for the component.
+        protected override System.Drawing.Bitmap Icon
         {
+            get
             {
-                this.m_attributes = (IGH_Attributes)new PSliderAttributes(this, this.probabilities);
+                // You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
             }
         }
 
-        //protected override void SolveInstance(IGH_DataAccess DA)
-        //{}
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("32fd8734-4bba-4b7d-9006-af3b38b889cb"); }
+        }
 
+        // create component display attributes
+        public override void CreateAttributes()
+        {
+            {
+                this.m_attributes = new POutputAttributes(this);
+            }
+        }
+
+        // update PD value
+        private void updatePD(object sender, string text)
+        {
+            this.tempPD = text;
+        }
+
+        // Call to update the PDF of this PSlider
+        public void updatePDF(List<double> p)
+        {
+            this.probabilities = p;
+        }
+
+        // histo grapgh toggle
+        public void menuItemHisto(object sender, EventArgs e)
+        {
+            this.draw_flag = "h";
+        }
+
+        // smooth graph toggle
+        public void menuItemSmooth(object sender, EventArgs e)
+        {
+            this.draw_flag = "s";
+        }
+
+        // append additional items in menu
         public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
         {
             menu.Items.Add("Histogram", null, menuItemHisto);
@@ -83,11 +101,11 @@ namespace Lab_Mouse.Components
             // additional dropdown menu for custom PD
             ToolStripMenuItem PDDropdown = GH_DocumentObject.Menu_AppendItem(menu, "Custom PD");
 
-            string displayText  = "";
-            for (int i =0; i < this.probabilities.Count; i++)
+            string displayText = "";
+            for (int i = 0; i < this.probabilities.Count; i++)
             {
                 displayText += this.probabilities[i].ToString();
-                if (i != this.probabilities.Count-1)
+                if (i != this.probabilities.Count - 1)
                 {
                     displayText += " , ";
                 }
@@ -106,15 +124,13 @@ namespace Lab_Mouse.Components
             base.AppendAdditionalMenuItems(menu);
         }
 
-
-        // Rui
         // parse input PD and update probability
         private void OK_Click(object sender, EventArgs e)
         {
             string[] values = this.tempPD.Split(',');
             List<double> tempPDList = new List<double>();
 
-            for (int i=0; i<values.Length; i++)
+            for (int i = 0; i < values.Length; i++)
             {
                 tempPDList.Add(Double.Parse(values[i], CultureInfo.InvariantCulture));
             }
@@ -124,129 +140,58 @@ namespace Lab_Mouse.Components
             ExpireSolution(true);
         }
 
-        // Rui
-        // cancel input even
+        // cancel input event
         private void Cancel_Click(object sender, EventArgs e)
         {
-
         }
 
-        // Rui
-        // store PD input
-        private void updatePD(object sender, string text)
+        // initialization 
+        public override void AddedToDocument(GH_Document document)
         {
-            this.tempPD = text;
+            base.AddedToDocument(document);
+            this.Attributes.Bounds = new RectangleF(this.Attributes.Pivot, new SizeF(160, 20));
         }
-
-        public void menuItemHisto(object sender, EventArgs e)
-        {
-            this.draw_flag = "h";
-        }
-
-        public void menuItemSmooth(object sender, EventArgs e)
-        {
-            this.draw_flag = "s";
-        }
-
-        // Call to update the PDF of this PSlider
-        public void updatePDF(List<double> p)
-        {
-            this.probabilities = p;
-        }
-
-        /// <summary>
-        /// Provides an Icon for every component that will be visible in the User Interface.
-        /// Icons need to be 24x24 pixels.
-        /// </summary>
-        protected override System.Drawing.Bitmap Icon
-        {
-            get
-            {
-                // You can add image files to your project resources and access them like this:
-                return Lab_Mouse.Properties.Resources.PSlider_icon;
-
-                //return null;
-            }
-        }
-
-        /// <summary>
-        /// Each component must have a unique Guid to identify it. 
-        /// It is vital this Guid doesn't change otherwise old ghx files 
-        /// that use the old ID will partially fail during loading.
-        /// </summary>
-        public override Guid ComponentGuid
-        {
-            get { return new Guid("44a1a9c7-0eef-4cb1-b6de-c30522eb0aa6"); }
-        }
-
-    }
-
-    // sets maximum height of histogram window above slider (global variable)
-    static class glob
-    {
-        public static int max_ht = 50;
     }
 
 
-    // this class overrides the classic slider to inherit it's properties and draw over it
-    public class PSliderAttributes : Grasshopper.Kernel.Special.GH_NumberSliderAttributes
+    // POutput component attributes
+    public class POutputAttributes : GH_ResizableAttributes<POutput>
     {
-
         private List<double> probabilities;
-        PSlider own;
-        // private float minimum;
+        POutput own;
 
-
-        public PSliderAttributes(PSlider owner, List<double> probabs) :
-          base(owner)
+        public POutputAttributes(POutput owner) : base(owner)
         {
-            //probabilities = owner.probabilities;
             own = owner;
         }
 
-
-        private float bins(float val)
+        // minimum size for resizing
+        protected override Size MinimumSize
         {
-
-            List<List<float>> bin_ranges = new List<List<float>>();
-            float max = (float)this.Owner.Slider.Maximum;
-            float min = (float)this.Owner.Slider.Minimum;
-            float bin_size = (max - min) / own.probabilities.Count;
-            float returned_p = 0;
-
-            float counter = min;
-            for (int i = 0; i < own.probabilities.Count; i++)
-            {
-
-                List<float> t = new List<float> { counter, counter + bin_size };
-                bin_ranges.Add(t);
-
-                counter = t[1];
-            }
-
-
-            for (int i = 0; i < bin_ranges.Count; i++)
-            {
-                if (val > bin_ranges[i][0] && val <= bin_ranges[i][1])
-                {
-                    returned_p = (float)own.probabilities[i];
-                }
-            }
-            return returned_p;
+            get { return new Size(160, 20); }
         }
 
-        // this function gets the coordinates from the probabilities, for drawing an irregular polygon from points 
+
+        protected override Size MaximumSize
+        {
+            get { return new Size(500, 20); }
+        }
+
+        // padding area for mouse responsing in order to resize
+        protected override Padding SizingBorders
+        {
+            get { return new Padding(0,0,10,0); }
+        }
+
+        // get points to draw polygon
         private PointF[] getPts(List<double> Probabilities)
 
         {
             int n = probabilities.Count + 4;
             PointF[] points = new PointF[n];
 
-            int width_nickname = GH_FontServer.StringWidth(Owner.NickName, GH_FontServer.Standard);
-
-
-            points[0] = new PointF(this.Pivot.X + width_nickname, this.Pivot.Y - 7);
-            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 11, this.Pivot.Y - 7);
+            points[0] = new PointF(this.Pivot.X , this.Pivot.Y - 7);
+            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
 
 
             if (probabilities.Count != 0)
@@ -258,7 +203,7 @@ namespace Lab_Mouse.Components
                     float bin_width = rail_width / Probabilities.Count;
                     float t = (Probabilities.Count - i) / Probabilities.Count;
 
-                    points[i + 3] = new PointF((float)((this.Pivot.X + width_nickname) + (bin_width * (Probabilities.Count - i)) - bin_width * 0.5), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
+                    points[i + 3] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i)) - bin_width * 0.5), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
                 }
             }
 
@@ -268,8 +213,6 @@ namespace Lab_Mouse.Components
             return points;
         }
 
-
-        // Rui
         // Calculate the background bays for the graph based on Probabilities 
         // return a list of equal size rectangles as background for the render
         private RectangleF[] getBackgroundBins(List<double> Probabilities)
@@ -279,10 +222,9 @@ namespace Lab_Mouse.Components
 
             PointF[] points = new PointF[n];
 
-            int width_nickname = GH_FontServer.StringWidth(Owner.NickName, GH_FontServer.Standard);
 
-            points[0] = new PointF(this.Pivot.X + width_nickname, this.Pivot.Y - 7);
-            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 11, this.Pivot.Y - 7);
+            points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
+            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
 
             if (Probabilities.Count != 0)
             {
@@ -292,7 +234,7 @@ namespace Lab_Mouse.Components
                     float bin_width = rail_width / Probabilities.Count;
 
                     backgroundBins[i] = new System.Drawing.RectangleF(
-                        (float)((this.Pivot.X + width_nickname) + (bin_width * (Probabilities.Count - i - 1))),
+                        (float)((this.Pivot.X ) + (bin_width * (Probabilities.Count - i - 1))),
                         (float)(this.Pivot.Y - 7 - glob.max_ht),
                         (float)bin_width,
                         (float)(glob.max_ht));
@@ -302,7 +244,6 @@ namespace Lab_Mouse.Components
             return backgroundBins;
         }
 
-
         // this function gets the coordinates from the probabilities, (same as getPts) but draws a HISTOGRAM shape an irregular polygon from points
         private PointF[] getHistoPts(List<double> Probabilities)
 
@@ -310,11 +251,8 @@ namespace Lab_Mouse.Components
             int n = (Probabilities.Count * 2) + 2;
             PointF[] points = new PointF[n];
 
-            int width_nickname = GH_FontServer.StringWidth(Owner.NickName, GH_FontServer.Standard);
-
-
-            points[0] = new PointF(this.Pivot.X + width_nickname, this.Pivot.Y - 7);
-            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 11, this.Pivot.Y - 7);
+            points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
+            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
 
 
             if (Probabilities.Count != 0)
@@ -327,8 +265,8 @@ namespace Lab_Mouse.Components
                     float bin_width = rail_width / Probabilities.Count;
                     float t = (Probabilities.Count - i) / Probabilities.Count;
 
-                    points[count + 2] = new PointF((float)((this.Pivot.X + width_nickname) + (bin_width * (Probabilities.Count - i))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
-                    points[count + 2 + 1] = new PointF((float)((this.Pivot.X + width_nickname) + (bin_width * (Probabilities.Count - i - 1))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
+                    points[count + 2] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
+                    points[count + 2 + 1] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i - 1))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
                     count += 2;
                 }
             }
@@ -336,22 +274,20 @@ namespace Lab_Mouse.Components
             return points;
         }
 
-
-        // Rui 
-        // double click handler on graph
+        // double click event handler
         public override GH_ObjectResponse RespondToMouseDoubleClick(GH_Canvas sender, GH_CanvasMouseEvent e)
         {
             System.Drawing.RectangleF[] rec = backgroundBinBounds;
 
-            for (int i = 0; i <rec.Length; i++)
+            for (int i = 0; i < rec.Length; i++)
             {
                 if (rec[i].Contains(e.CanvasLocation))
                 {
                     int pos = rec.Length - i - 1;
 
-                    for (int j =0; j < own.probabilities.Count; j ++)
+                    for (int j = 0; j < own.probabilities.Count; j++)
                     {
-                        if (j != pos )
+                        if (j != pos)
                         {
                             own.probabilities[j] = 0;
                         }
@@ -369,48 +305,53 @@ namespace Lab_Mouse.Components
                     return GH_ObjectResponse.Handled;
                 }
             }
-            
+
             return base.RespondToMouseDoubleClick(sender, e);
         }
 
-        // Rui
-        // new layout 
+        // component bound wrapper
+        private System.Drawing.RectangleF[] backgroundBinBounds { get; set; }
+
         private RectangleF _baseBounds;
-        private RectangleF _thisBounds;
         private RectangleF _extraBounds;
+        private RectangleF _thisBounds;
 
         protected override void Layout()
         {
             base.Layout();
-
             _baseBounds = Bounds;
 
             _extraBounds = Bounds;
-            _extraBounds.Y -= 60;
-            _extraBounds.Height += 60;
+            _extraBounds.Y = Bounds.Top - 60;
+            _extraBounds.Height = 80;
 
-            _thisBounds = RectangleF.Union(_baseBounds, _extraBounds);
-
-            Bounds = _thisBounds;
+            // Overwrite the Bounds property to include our external button.
         }
 
-        // Rui
-        // list of background bin Bounds
-        private System.Drawing.RectangleF[] backgroundBinBounds { get; set;}
-
-
-        // this function takes care of the drawing routines 
-        protected override void Render(Grasshopper.GUI.Canvas.GH_Canvas canvas, Graphics graphics,
-          Grasshopper.GUI.Canvas.GH_CanvasChannel channel)
+        // render
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
         {
-
             if (channel != Grasshopper.GUI.Canvas.GH_CanvasChannel.Objects)
                 return;
 
-            // render of original component 
-            Bounds = _baseBounds;
-            base.Render(canvas, graphics, channel);
-            Bounds = _thisBounds;
+            GH_Palette palette = GH_Palette.Normal;
+
+            //base.Render(canvas, graphics, channel);
+
+            // Create a new Capsule 
+            GH_Capsule capsule = GH_Capsule.CreateCapsule(_baseBounds, palette);
+            capsule.AddInputGrip(this.InputGrip.X, this.InputGrip.Y);
+            capsule.AddOutputGrip(this.OutputGrip.X, this.OutputGrip.Y);
+
+            //Render the capsule using the current Selection, Locked and Hidden states.
+            //Integer parameters are always hidden since they cannot be drawn in the viewport.
+            capsule.Render(graphics, Selected, Owner.Locked, true);
+
+            //Always dispose of a GH_Capsule when you're done with it.
+            capsule.Dispose();
+            capsule = null;
+
+            Bounds = _extraBounds;
 
             int width = GH_FontServer.StringWidth(Owner.NickName, GH_FontServer.Standard);
             PointF p = new PointF(this.Pivot.X + width + 19, this.Pivot.Y - 7);
@@ -439,7 +380,6 @@ namespace Lab_Mouse.Components
 
             maxPt = pts[index_max];
 
-
             // Create Pens
             System.Drawing.Pen pen2 = new System.Drawing.Pen(Brushes.Red, 2);
             System.Drawing.Pen pen = new System.Drawing.Pen(Brushes.Black, 2);
@@ -463,15 +403,6 @@ namespace Lab_Mouse.Components
             graphics.DrawPolygon(pen, pts);
             graphics.FillPolygon(lb, pts);
 
-            //Owner.NickName = "Variable";
-
-            // Draw probability value
-            // string evidence = "Y<5
-
-            string s = "P(MINIMIZE Y)=" + (this.bins((float)Owner.CurrentValue) * 100).ToString() + "%";
-            //string s = (Owner.CurrentValue + 1).ToString();
-            //double cv = Owner.CurrentValue;
-            graphics.DrawString(s, GH_FontServer.Standard, Brushes.Black, pts[1].X + 15, (int)(this.Pivot.Y - 8 - glob.max_ht));
         }
     }
 }
